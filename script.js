ef61a7770a6e81a7970d8987b90f7dbd026f1e5d
Yahya-Ozair <yahyaozair1@gmail.com>
2026-03-14 23:50:10 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+body{
+font-family: Arial;
+margin:0;
+background:#f2f2f2;
+}
+
+header{
+background:#4CAF50;
+color:white;
+padding:20px;
+text-align:center;
+}
+
+nav{
+background:#333;
+padding:10px;
+text-align:center;
+}
+
+nav a{
+color:white;
+margin:10px;
+text-decoration:none;
+}
+
+.container{
+display:grid;
+grid-template-columns:repeat(auto-fit,minmax(250px,1fr));
+gap:20px;
+padding:20px;
+}
+
+.card{
+background:white;
+padding:20px;
+border-radius:10px;
+box-shadow:0 4px 10px rgba(0,0,0,0.1);
+}
+
+.profile{
+text-align:center;
+margin:20px;
+}
